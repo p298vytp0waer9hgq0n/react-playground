5819d8dd756c1533dbfb2c4aa2ea1f03131dad06
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2022-08-02 18:11:27 +0500

    feat: props for cards

--- src/components/Airbnb/Cards.js
+++ src/components/Airbnb/Cards.js
@@ -1,17 +1,22 @@
 import Card from './Card';
-import wedding from '../../images/wedding.png';
+import { data } from './data';
 
 export default function Cards () {
+    const cardElements = data.map((item) => {
+        return <Card
+            key={item.id}
+            img={item.coverImg}
+            title={item.title}
+            rating={item.stats.rating}
+            reviewCount={item.stats.reviewCount}
+            country={item.location}
+            price={item.price}
+            openSpots={item.openSpots}
+        />;
+    });
     return (
         <section className="air-cards">
-            <Card 
-                img={wedding}
-                rating="5.0"
-                reviewCount={6}
-                country="Norway"
-                title="Learn wedding photography"
-                price={134}
-            />
+            {cardElements}
         </section>
     )
 }
---
+++ src/components/Airbnb/data.js
@@ -0,0 +1,59 @@
+import wedding from '../../images/wedding.png';
+import knight from '../../images/knight.png';
+import engeneering from '../../images/engeneering.png';
+import coding from '../../images/coding.png';
+
+export const data = [
+    {
+        id: 1,
+        title: 'Learn Wedding Photography',
+        description: 'Provide the couple with memories about how they ruined their damn lifes!',
+        price: 125,
+        coverImg: wedding,
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: 'Online',
+        openSpots: 0 
+    },
+    {
+        id: 2,
+        title: 'Get Gud',
+        description: 'Dozens of hours of exquisit suffering from Soul Tyrant and shit!',
+        price: 2,
+        coverImg: knight,
+        stats: {
+            rating: 1.0,
+            reviewCount: 300
+        },
+        location: 'Home. Always...',
+        openSpots: 24000
+    },
+    {
+        id: 3,
+        title: 'Civil Engeneering Degree',
+        description: 'Safety first!',
+        price: 0.5,
+        coverImg: engeneering,
+        stats: {
+            rating: 4.0,
+            reviewCount: 20
+        },
+        location: 'Home again',
+        openSpots: 24000
+    },
+    {
+        id: 4,
+        title: 'Learn Bloody React',
+        description: 'The only music you need is the screeching of your brain!',
+        price: 300,
+        coverImg: coding,
+        stats: {
+            rating: 5.0,
+            reviewCount: 100
+        },
+        location: 'Online',
+        openSpots: 70 
+    }
+]
