dc4acc04e2f0d9e49d7db7ef1faca133f36eca4c
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2023-09-09 01:51:37 +0500

    bleh

--- src/components/ReactFacts/Main.tsx
+++ src/components/ReactFacts/Main.tsx
@@ -1,4 +1,21 @@
-export default function Main () {
+function capitalizeEveryOther(str: string) {
+    let res = "";
+    let count = 0;
+    for (const l of str) {
+        count++;
+        res += (count & 1) === 0 ? l.toUpperCase() : l.toLowerCase();
+    }
+    return res;
+    /* return Array.from(str).reduce(
+        (acc, letter, index) =>
+            (index & 1) === 0
+                ? acc + letter.toLowerCase()
+                : acc + letter.toUpperCase(),
+        ""
+    ); */
+}
+
+export default function Main() {
     return (
         <main className="content">
             <h1 className="content__title">Fun facts about React</h1>
@@ -6,9 +23,12 @@ export default function Main () {
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile&nbsp;apps</li>
+                <li>{capitalizeEveryOther("Is maintained by Facebook")}</li>
+                <li>
+                    Powers thousands of enterprise apps, including
+                    mobile&nbsp;apps
+                </li>
             </ul>
         </main>
-    )
+    );
 }
